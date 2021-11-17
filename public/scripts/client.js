8d7a12ee9881e7e7dfe71ddcb1d9435464b903c4
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-11-17 18:50:39 -0500

    Commit of everything done up to this point because I forgot to do so previously.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,63 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+$(document).ready(function() {
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      let newTweet = createTweetElement(tweet);
+      $('.tweets').prepend(newTweet);
+    }
+  };
+  const loadTweets = function() {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      dataType: 'json',
+      success: function(res) {
+        renderTweets(res);
+      }
+    });
+  };
+
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  
+
+  const createTweetElement = function(tweets) {
+    const date = timeago.format(tweets.created_at);
+    const article = '<article class="tweet">';
+    const header = `<header class="tweetHeader"><span class="name"><i class="fas fa-smile-beam paddingRight"></i>${escape(tweets.user.name)}</span><span class="handle">${escape(tweets.user.handle)}</span></header>`;
+    const body = `<div class="content"><span>${escape(tweets.content.text)}</span></div>`;
+    const footer = `<footer class="tweetFooter"><div class="underline"><span class="time paddingTop">${date}</span><span class="buttons paddingTop"><i class="fas fa-flag flag"></i><i class="fas fa-retweet retweet"></i><i class="fas fa-heart favourite"></i></span></div></footer>`;
+    const $tweet = $(`${article}${header}${body}${footer}</article>`);
+    return $tweet;
+  };
+
+  $("#form-submit-tweet").submit(function(event) {
+    const formData = $(this).serialize();
+    const tweetTextLength = $("#tweet-text").val().length;
+    event.preventDefault();
+    $("#error").slideUp("fast");
+    if (tweetTextLength < 140 && tweetTextLength > 0) {
+    $.ajax('/tweets/', {
+      method: 'POST',
+      data: formData
+    })
+    .then(function () {
+      loadTweets();
+    });
+  } else if (tweetTextLength > 140) {
+    $("#error").html("<p>Tweet too long!</p>").slideDown("slow");
+  } else if (tweetTextLength === 0) {
+    $("#error").html("<p>Tweet is empty! Try entering some text.</p>").slideDown("slow");
+  }
+  });
+
+  loadTweets();
+});
+
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $(".placeholder").on("input", function() {
+    let character = $(this).val().length;
+    const parent = $(this).parent();
+    let counter = $(parent).children().children("output").eq(0)["0"]
+    counter.innerHTML = 140 - character;
+
+    if (counter.innerHTML < 0) {
+      counter.style.color = "#FF0000";
+    }
+    if (counter.innerHTML >= 0) {
+      counter.style.color = "#545149";
+    }
+  });
+});
